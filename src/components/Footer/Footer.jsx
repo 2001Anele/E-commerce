785e425f167ec82ce840859e0779ebf89e07830c
Ntombikayise Anele Xaba <Ntombikayiseax@dutappfactory.tech>
2023-05-29 15:00:11 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,25 +25,71 @@ const Footer = () => {
                         <h4 className="quick__links-title">
                             Top Categories
                         </h4>
-                        <ListGroup>
-                            <ListGroupItem>
+                        <ListGroup className="mb-3">
+                            <ListGroupItem className="ps-0 border-0">
                                 <Link to='#'>Dishwashing Liquids</Link>
                             </ListGroupItem>
 
-                            <ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
                                 <Link to='#'>Bleach Cleaners</Link>
                             </ListGroupItem>
-                            <ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
                                 <Link to='#'>Bathroom Cleaners</Link>
                             </ListGroupItem>
-                            <ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
                                 <Link to='#'>Brooms, Dustpans & Brushes</Link>
                             </ListGroupItem>
                         </ListGroup>
                     </div>
                 </Col>
-                <Col lg="2"></Col>
-                <Col lg="3"></Col>
+                <Col lg="2">
+                <div className="quick__links">
+                        <h4 className="quick__links-title">
+                            Useful Links
+                        </h4>
+                <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                                <Link to="/shop">Shop</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
+                                <Link to="/cart">Cart</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
+                                <Link to="/login">Login</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
+                                <Link to='#'>Privacy Policy</Link>
+                            </ListGroupItem>
+                        </ListGroup>
+                        </div>
+
+                </Col>
+                <Col lg="3">
+                <div className="quick__links">
+                        <h4 className="quick__links-title">
+                            Useful Links
+                        </h4>
+                <ListGroup className="mb-3">
+                            <ListGroupItem className="ps-0 border-0">
+                                <span><i class="ri-map-pin-line"></i></span>
+                                <p>259 Umdlebe Rd, Umlazi, Durban</p>
+                            </ListGroupItem>
+
+                            <ListGroupItem className="ps-0 border-0">
+                            <span><i class="ri-phone-line"></i></span>
+                                <p>+27 833 444 275</p>
+                            </ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
+                            <span><i class="ri-map-pin-line"></i></span>
+                                <p>259 Umdlebe Rd, Umlazi, Durban</p>
+                            </ListGroupItem>
+                            <ListGroupItem className="ps-0 border-0">
+                            <span><i class="ri-mail-line"></i></span>
+                                <p>pongolasupplies@gmail.com</p>
+                            </ListGroupItem>
+                        </ListGroup>
+                        </div>
+                </Col>
             </Row>
         </Container>
     </footer>
